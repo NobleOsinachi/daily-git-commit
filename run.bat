a6937061ff6546dce49c6669f0881d31a5309060
Noble Osinachi <nobleosinachi98@gmail.com>
2023-04-13 21:00:36 +0100

    Last updated on Thursday April 13 2023 at 09:00:10 PM
    Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
    Co-authored-by: David Popoola <davieedpopoola@gmail.com>
    Co-authored-by: Golden Chukwukere <66950981+iamGolden@users.noreply.github.com>
    Co-authored-by: Ibukun <ibukunoladipupo10@gmail.com>
    Co-authored-by: Isaac Bassey <chartobyson2025@gmail.com>
    Co-authored-by: Jato Joseph <jaotjoseph4@gmail.com>
    Co-authored-by: Joan <sylvesterjoannaunyii@gmail.com>
    Co-authored-by: Joshua-Chris <joshuadgreat04@gmail.com>
    Co-authored-by: Noble Osinachi <150407018@live.unilag.edu.ng>
    Co-authored-by: PraiseGeek <praisegeek@gmail.com>
    Co-authored-by: Precious Adedibu <adedibuprecious@gmail.com>
    Co-authored-by: Sanowar Sunny <sanowarhossain3843@gmail.com>
    Co-authored-by: iAmGolden <chuksgold29@gmail.com>
    Co-authored-by: josiahking <josiahaccounts@gmail.com>
    Co-authored-by: masaun <masaun@protonmail.com>
    Co-authored-by: femotizo <epebinuoluwafemi@gmail.com>
    Signed-off-by: Noble Osinachi <nobleosinachi98@gmail.com>

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 cd "C://Users//Noble//SOURCES//REPOS//daily-git-commit" & "C://Users//Noble//AppData//Local//Programs//Python//Python38-32//python.exe" "C://Users//Noble//SOURCES//REPOS//daily-git-commit//main.py"
 
-cd "C://Users//Noble//SOURCES//REPOS//NobleOsinachi//"
+@REM cd "C://Users//Noble//SOURCES//REPOS//NobleOsinachi//" && node create-pr.js
 
-node create-pr.js
+cd "C:\Users\Noble\SOURCES\REPOS\auto-commit-all-repos" && npm start
