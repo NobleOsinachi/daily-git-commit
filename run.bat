002628171b609851feba7886963384425233f144
Noble Osinachi <nobleosinachi98@gmail.com>
2023-04-13 23:24:22 +0100

    Last updated on Thursday April 13 2023 at 11:23:58 PM
    Co-authored-by: David Oluwasegun <oluwasegundavid7@gmail.com>
    Co-authored-by: David Popoola <davieedpopoola@gmail.com>
    Co-authored-by: Golden Chukwukere <66950981+iamGolden@users.noreply.github.com>
    Co-authored-by: Ibukun <ibukunoladipupo10@gmail.com>
    Co-authored-by: Isaac Bassey <chartobyson2025@gmail.com>
    Co-authored-by: Jato Joseph <jaotjoseph4@gmail.com>
    Co-authored-by: Joan <sylvesterjoannaunyii@gmail.com>
    Co-authored-by: Joshua-Chris <joshuadgreat04@gmail.com>
    Co-authored-by: Noble Osinachi <150407018@live.unilag.edu.ng>
    Co-authored-by: PraiseGeek <praisegeek@gmail.com>
    Co-authored-by: Precious Adedibu <adedibuprecious@gmail.com>
    Co-authored-by: Sanowar Sunny <sanowarhossain3843@gmail.com>
    Co-authored-by: iAmGolden <chuksgold29@gmail.com>
    Co-authored-by: josiahking <josiahaccounts@gmail.com>
    Co-authored-by: masaun <masaun@protonmail.com>
    Co-authored-by: femotizo <epebinuoluwafemi@gmail.com>
    Signed-off-by: Noble Osinachi <nobleosinachi98@gmail.com>

--- run.bat
+++ run.bat
@@ -1,5 +1,13 @@
-cd "C://Users//Noble//SOURCES//REPOS//daily-git-commit" & "C://Users//Noble//AppData//Local//Programs//Python//Python38-32//python.exe" "C://Users//Noble//SOURCES//REPOS//daily-git-commit//main.py"
+@REM cd "C://Users//Noble//SOURCES//REPOS//daily-git-commit" & "C://Users//Noble//AppData//Local//Programs//Python//Python38-32//python.exe" "C://Users//Noble//SOURCES//REPOS//daily-git-commit//main.py"
 
 @REM cd "C://Users//Noble//SOURCES//REPOS//NobleOsinachi//" && node create-pr.js
 
-cd "C://Users//Noble//SOURCES//REPOS//auto-commit-all-repos" && npm start
+@REM cd "C://Users//Noble//SOURCES//REPOS//auto-commit-all-repos" && npm start
+
+
+
+start /B "" cmd /C "cd /d C:\Users\Noble\SOURCES\REPOS\daily-git-commit && C:\Users\Noble\AppData\Local\Programs\Python\Python38-32\python.exe main.py"
+
+@REM cd C:\Users\Noble\SOURCES\REPOS\NobleOsinachi && node create-pr.js
+
+start /B "" cmd /C "cd /d C:\Users\Noble\SOURCES\REPOS\auto-commit-all-repos && npm start"
